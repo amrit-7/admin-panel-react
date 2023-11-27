4b8e7ac5d84d67f8f3fac74f8e504413685bf3ab
amrit <gillamrit29@gmail.com>
2023-11-27 13:11:16 +0530

    navlinks

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -22,24 +22,24 @@ const Dashboard = () => {
       <div className="row">
         <div className="col">
           <div className="card shadow-sm">
-            <div className="card-header">Admins</div>
+            <div className="card-header">Tutors</div>
             <div className="card-body">
-              <h5 className="card-title">5 Admins in total</h5>
-              <p className="card-text">Check and manage admins</p>
-              <a href="/admin/list" className="btn btn-dark">
-                See Admins
+              <h5 className="card-title">5 Tutors in total</h5>
+              <p className="card-text">Check and manage Tutors</p>
+              <a href="/tutors" className="btn btn-dark">
+                See Tutors
               </a>
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card shadow-sm">
-            <div className="card-header">Users</div>
+            <div className="card-header">Students</div>
             <div className="card-body">
-              <h5 className="card-title">106 Users in total</h5>
-              <p className="card-text">Check and manage users</p>
-              <a href="#" className="btn btn-dark">
-                See Users
+              <h5 className="card-title">106 Students in total</h5>
+              <p className="card-text">Check and manage Students</p>
+              <a href="/students" className="btn btn-dark">
+                See Students
               </a>
             </div>
           </div>
